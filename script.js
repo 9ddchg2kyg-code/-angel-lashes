8f4ff9136161d41867c78ae7c6aa0e2c791b379c
9ddchg2kyg-code <9ddchg2kyg@privaterelay.appleid.com>
2026-07-27 00:59:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const images = document.querySelectorAll(".gallery img");
 
-images.forEach(img => {
+images.forEach(image => {
 
-img.addEventListener("click", () => {
+image.addEventListener("click", () => {
 
 const overlay = document.createElement("div");
 
@@ -13,24 +13,26 @@ overlay.style.top = "0";
 overlay.style.left = "0";
 overlay.style.width = "100%";
 overlay.style.height = "100%";
-overlay.style.background = "rgba(0,0,0,.9)";
+overlay.style.background = "rgba(0,0,0,.92)";
 overlay.style.display = "flex";
 overlay.style.justifyContent = "center";
 overlay.style.alignItems = "center";
-overlay.style.zIndex = "9999";
+overlay.style.zIndex = "99999";
 overlay.style.cursor = "pointer";
 
 const photo = document.createElement("img");
 
-photo.src = img.src;
+photo.src = image.src;
 photo.style.maxWidth = "92%";
 photo.style.maxHeight = "92%";
-photo.style.borderRadius = "20px";
-photo.style.boxShadow = "0 20px 60px rgba(0,0,0,.45)";
+photo.style.borderRadius = "18px";
+photo.style.boxShadow = "0 20px 60px rgba(0,0,0,.4)";
 
 overlay.appendChild(photo);
 
-overlay.onclick = () => overlay.remove();
+overlay.addEventListener("click", () => {
+overlay.remove();
+});
 
 document.body.appendChild(overlay);
 
